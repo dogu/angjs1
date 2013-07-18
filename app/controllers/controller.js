8a9cfeff706ae4a18e8f1bf04d7b4c5b4e8ba859
Sindre Haaland <sindre@dogu.no>
2013-07-18 16:21:29 +0200

    Legg til og fjern melding

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -2,17 +2,41 @@ function FirstCtrl($scope, DataSource) {
 
     $scope.data = DataSource;
 
-
     function updatePercent() {
         DataSource.completed = DataSource.messages.length * 10;
     }
 
+    $scope.updateMessage = function () {
+        // Get ID of message to update
+        var idToUpdate = DataSource.messages[0].id;
+        // Get the object from the database
+        DataSource.Db.get({id: idToUpdate}, function (data) {
+            // On success, update the text
+            data.txt = $scope.data.message;
+            // Store it to the database
+            data.$update();
+        });
+    }
+
     $scope.addMessage = function () {
-        DataSource.messages.push({txt: $scope.data.message});
-        updatePercent();
+        // Store a new message to the database, id is automatically assigned
+        var msg = DataSource.Db.save({name: $scope.data.name, txt: $scope.data.message}, function (data) {
+            // Update the datasource messages to get new messages
+            DataSource.messages = DataSource.Db.query({}, function () {
+                updatePercent();
+
+            });
+            // OR add only one, but then without id :( ...
+            //DataSource.messages.push(new DataSource.Db({txt:$scope.data.message}));
+
+        });
+
+
     }
 
     $scope.removeMessage = function () {
+        var id = $scope.data.messages[$scope.data.messages.length - 1].id;
+        DataSource.Db.delete({id: id});
         DataSource.messages.splice(DataSource.messages.length - 1);
         updatePercent();
     }
--- app/services/DataSource.js
+++ app/services/DataSource.js
@@ -4,15 +4,23 @@ app.factory("DataSource", function ($resource, $q) {
     var percentagePromise = $q.defer();
 
     // Connect to the MongoDB Rest Backend using a AngularJS $resource REST-service object
-    var Db = $resource('http://cpanel02.cloudapp.net\\:28000/Rest2012/angjs1/:collection/:id',
-        {collection: '@collection', id: '@id'});
+    var Db = $resource('http://cpanel02.cloudapp.net\\:28000/Rest2012/angjs1/messages/:id',
+        {id: '@id'},
+        {
+            update: {
+                method: 'PUT'
+            },
+            delete: {
+                method: 'DELETE'
+            }
+        });
 
     // Get all messages from the rest service
-    var messages = Db.query({collection: 'messages'}, function (data) {
+    var messages = Db.query({}, function (data) {
         // Resolve the promise with the new percentage
         var percentage = data.length * 10;
         percentagePromise.resolve(percentage);
     });
 
-    return { messages: messages, completed: percentagePromise.promise}
+    return { Db: Db, messages: messages, completed: percentagePromise.promise}
 })
