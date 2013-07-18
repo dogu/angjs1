0f5c797fb5b9b236b3f17336b573fe4da44c21e0
Sindre Haaland <sindre@dogu.no>
2013-07-18 19:03:50 +0200

    La til meny

--- app/app.js
+++ app/app.js
@@ -2,12 +2,17 @@ var app = angular.module('app', ['ngResource', 'ui.bootstrap']);
 
 // #### ROUTING ####
 app.config(function ($routeProvider) {
-    $routeProvider.when("/twitter/:twitterScreenName", {
+    $routeProvider.when("/", {
+        templateUrl:"home.html"
+    }).when("/messages", {
+        templateUrl: "messages.html",
+        controller: "FirstCtrl"
+    }).when("/twitter/:twitterScreenName", {
         templateUrl: "twitter.html",
         controller: "TwitterCtrl"
     }).when("/twitter", {
-            redirectTo: "/twitter/Dogu_AS/"
-        }).otherwise({
-            redirectTo: "/"
-        })
+        redirectTo: "/twitter/Dogu_AS/"
+    }).otherwise({
+        redirectTo: "/"
+    })
 });
